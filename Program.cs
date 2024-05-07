565cb3c119357c21190bb0d94fb3314541b0ab95
assasinww <89029903+assasinww@users.noreply.github.com>
2024-05-07 08:04:29 +0100

    Error uploading

--- Program.cs
+++ Program.cs
@@ -1,5 +1,17 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+// Configure MIME types
+app.UseStaticFiles(new StaticFileOptions
+{
+    ServeUnknownFileTypes = true,
+    DefaultContentType = "application/octet-stream",
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, "wwwroot")),
+    RequestPath = "/your-unity-files-path" // Adjust this path according to your Unity WebGL files' location
+});
 
 app.MapGet("/{*path}", (HttpContext context) =>
 {
@@ -7,10 +19,9 @@ app.MapGet("/{*path}", (HttpContext context) =>
     context.Response.ContentType = "text/html";
     return context.Response.SendFileAsync(Path.Combine(builder.Environment.ContentRootPath, "wwwroot", "index.html"));
 });
-app.UseRouting();
 
+app.UseRouting();
 app.UseDefaultFiles();
 app.UseStaticFiles();
-    
 
 app.Run();
